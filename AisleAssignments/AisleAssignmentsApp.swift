bc94f71045c37ef1a8826bf3583682df5f5b4c3f
Pavan Javali <pavan.javali@globaldelight.com>
2025-07-17 22:23:23 +0530

    Initial Commit

---
+++ AisleAssignments/AisleAssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisleAssignmentsApp.swift
+//  AisleAssignments
+//
+//  Created by Pavan Javali on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AisleAssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
